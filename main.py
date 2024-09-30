f2bdc5d12e4e752e3a48c83479df645a03dca128
nathan <162269397+nperekovich@users.noreply.github.com>
2024-09-30 14:50:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#assignment/hw notes practice
+from assign_notes_practice import sept15
+from assign_notes_practice import sept23
+
+print("\n")
+
+#class practice/exercises
+from class_practice import sept11
+
+print("\n")
+
+from class_practice import sept19
