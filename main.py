9050a53aec4d2a4b8a09614457f9d0180323acb4
nathan <162269397+nperekovich@users.noreply.github.com>
2024-10-09 14:32:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #assignment/hw notes practice
 from assign_notes_practice import sept15
+
+print("\n")
+
 from assign_notes_practice import sept23
 
 print("\n")
@@ -10,4 +13,11 @@ from class_practice import sept11
 print("\n")
 
 from class_practice import sept19
+
+print("\n")
+
 from class_practice import sept30
+
+print("\n")
+
+from class_practice import oct7
