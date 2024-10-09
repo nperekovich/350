72d6886db924a6f2962f3fe292c49706e09b778d
nathan <162269397+nperekovich@users.noreply.github.com>
2024-10-09 14:31:33 -0500

    Create oct7.py

---
+++ class_practice/oct7.py
@@ -0,0 +1,27 @@
+#lista - fav world cities
+listA = ["tokyo","akihabara","seoul"]
+
+#listb fav usa cities
+listB = ["brooklyn","chicago","milwaukee","niles"]
+
+#listc fav foods
+listC = ["banana","watermelon","peach","blueberry","coffee"]
+
+#listd lanugages speak/want to speak
+listD = ["english","japanese","korean","spanish"]
+
+#liste fav books/songs/movies
+listE = ["d.gray-man","jujutsu kaisen","gintama","sakamoto days"]
+
+#listf fav restaurants/stores
+listF = ["seoul RIP","pilcrow","interval","jalisco's"]
+
+#listg fav friends/family/others
+listG = ["alanna","missy","lily","mom","brother"]
+
+#listh fav expressions/greetings when seeing someone
+listH = ["hello","yo","waddup"]
+
+#print madlib
+print("One of my favorite foods is " + ListC[0] +
+      " and one of my favorite coffee shops is " + ListF[1] + ".")
