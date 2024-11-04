fbf1b453eb0b8ed081493a8c04f9d047b9672108
nathan <162269397+nperekovich@users.noreply.github.com>
2024-11-04 14:26:11 -0600

    Create nov3.py

---
+++ assign_notes_practice/nov3.py
@@ -0,0 +1,80 @@
+'''
+chapter 7
+'''
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+#break
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+#continue
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+#else
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+'''
+for
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+for x in "banana":
+  print(x)
+#break
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+    fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+#continue
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+#range
+for x in range(6):
+  print(x)
+for x in range(2, 6):
+  print(x)
+for x in range(2, 30, 3):
+  print(x)
+#else
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+#nested loops
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+#pass
+for x in [0, 1, 2]:
+  pass
